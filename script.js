4ce6808e16fae4aef1a0bd1267e96c4daab7046f
ValPawsOnHips <hypnozeta12@gmail.com>
2024-01-25 10:10:16 -0600

    Finished

---
+++ script.js
@@ -0,0 +1,28 @@
+// const
+
+let num = 20;
+
+num = 15;
+
+num += 7;
+// OR: num = num + 7;
+
+num--;
+
+num = "This is not a number";
+
+let blank;
+
+blank = null;
+
+// BONUS
+const quarter = 0.25,
+  dime = 0.1,
+  nickle = 0.05,
+  penny = 0.01;
+
+console.log(quarter, dime, nickle, penny);
+
+let letters = "qwertyuiop";
+
+console.log(letters[letters.length - 1]);
